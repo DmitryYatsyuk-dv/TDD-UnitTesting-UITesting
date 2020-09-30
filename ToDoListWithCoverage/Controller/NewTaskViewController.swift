579f329d10b1afa2b44602900c7dfdc0771317b0
DmitryYatsyuk-dv <yatsyuk.dev@mail.ru>
2020-09-30 20:51:10 +0300

    Trying to get coordinates from Geocoder

--- ToDoListWithCoverage/Controller/NewTaskViewController.swift
+++ ToDoListWithCoverage/Controller/NewTaskViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import CoreLocation
 
 class NewTaskViewController: UIViewController {
+    
+    var taskManager: TaskManager!
+    var geocoder = CLGeocoder()
 
     //MARK: IBOutlets
     @IBOutlet var titleTextField: UITextField!
@@ -19,6 +23,25 @@ class NewTaskViewController: UIViewController {
     @IBOutlet var saveButton: UIButton!
     @IBOutlet var cancelButton: UIButton!
     
+    @IBAction func save() {
+        guard let titleString = titleTextField.text else { return }
+        guard let locationString = locationTextField.text else { return }
+        let date = dateFormatter.date(from: dateTextField.text!)
+        let descriptionString = descriptionTextField.text
+        guard let addressString = addressTextField.text else { return }
+        geocoder.geocodeAddressString(addressString) { [unowned self] (placemarks, error) in
+            let placemark = placemarks?.first
+            let coordinate = placemark?.location?.coordinate
+            let location = Location(name: locationString, coordinate: coordinate)
+            
+            let task = Task(title: titleString, description: descriptionString, date: date, location: location)
+            self.taskManager.add(task: task)
+    }
+}
     
-    
+    var dateFormatter: DateFormatter {
+        let df = DateFormatter()
+        df.dateFormat = "dd.MM.yy"
+        return df
+    }
 }
--- ToDoListWithCoverageTests/Controller/NewTaskViewControllerTests.swift
+++ ToDoListWithCoverageTests/Controller/NewTaskViewControllerTests.swift
@@ -7,11 +7,14 @@
 //
 
 import XCTest
+import CoreLocation
 @testable import ToDoListWithCoverage
+
 
 class NewTaskViewControllerTests: XCTestCase {
     
     var sut: NewTaskViewController!
+    var placemark: MockCLPlacemark!
         
     override func setUpWithError() throws {
         
@@ -51,6 +54,64 @@ class NewTaskViewControllerTests: XCTestCase {
         XCTAssertTrue(sut.cancelButton.isDescendant(of: sut.view))
     }
     
+    func testSaveUsesGeocoderToConvertCoordinatefromAddress() {
+        
+        let df = DateFormatter()
+        df.dateFormat = "dd.MM.yy"
+        let date = df.date(from: "9/30/20")
+        
+        sut.titleTextField.text = "Foo"
+        sut.locationTextField.text = "Bar"
+        sut.dateTextField.text = "9/30/20"
+        sut.addressTextField.text = "Moscow"
+        sut.descriptionTextField.text = "Baz"
+        
+        sut.taskManager = TaskManager()
+        let mockGeocoder = MockCLGeocoder()
+        sut.geocoder = mockGeocoder
+        sut.save()
+        
+        let coordinate = CLLocationCoordinate2D(latitude: 55.7615902, longitude: 37.60946)
+        let location = Location(name: "Bar", coordinate: coordinate)
+        let generatedTask = Task(title: "Foo", description: "Baz", date: date, location: location)
+        
+        placemark = MockCLPlacemark()
+        placemark.mockCoordinate = coordinate
+        mockGeocoder.completionHandler?([placemark], nil)
+        
+        let task = sut.taskManager.task(at: 0)
+        
+        XCTAssertEqual(task, generatedTask)
+    }
     
+    func testSaveButtonHasSaveMethod() {
+        let saveButton = sut.saveButton
+        
+        guard let actions = saveButton?.actions(forTarget: sut, forControlEvent: .touchUpInside) else {
+            XCTFail()
+            return
+        }
+        XCTAssertTrue(actions.contains("save"))
+    }
     
+}
+
+extension NewTaskViewControllerTests {
+    
+    class MockCLGeocoder: CLGeocoder {
+        
+        var completionHandler: CLGeocodeCompletionHandler?
+        
+        override func geocodeAddressString(_ addressString: String, completionHandler: @escaping CLGeocodeCompletionHandler) {
+            self.completionHandler = completionHandler
+        }
+    }
+    class MockCLPlacemark: CLPlacemark {
+        
+        var mockCoordinate: CLLocationCoordinate2D!
+        
+        override var location: CLLocation? {
+            return CLLocation(latitude: mockCoordinate.latitude, longitude: mockCoordinate.longitude)
+        }
+    }
 }
